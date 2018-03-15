ea007e19946b0f1697661ba7e2fca9ea1d69c81d
WillieT1986 <WarMad2@Hotmail.com>
2018-03-15 13:41:35 -0400

    Filled out StateController

--- src/main/java/org/wecancodeit/restingcitiesstates/StateController.java
+++ src/main/java/org/wecancodeit/restingcitiesstates/StateController.java
@@ -1,5 +1,28 @@
 package org.wecancodeit.restingcitiesstates;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class StateController {
+
+	@Resource
+	StateRepository stateRepo;
+
+	@RequestMapping("/show-states")
+	public String findAllState(Model model) {
+		model.addAttribute("states", stateRepo.findAll());
+		return "states";
+	}
+
+	@RequestMapping("/state")
+	public String findOneState(@RequestParam(value = "id") Long id, Model model) {
+		model.addAttribute("state", stateRepo.findOne(id));
+		return "state";
+	}
 
 }
