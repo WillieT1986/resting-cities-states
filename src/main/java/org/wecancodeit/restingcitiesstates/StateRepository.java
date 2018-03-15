8587399273d39b603faa7721d2e0fc19ccc51812
WillieT1986 <WarMad2@Hotmail.com>
2018-03-15 13:54:26 -0400

    Filled out rest controller, added iterables to repository

--- src/main/java/org/wecancodeit/restingcitiesstates/StateRepository.java
+++ src/main/java/org/wecancodeit/restingcitiesstates/StateRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface StateRepository extends CrudRepository<State, Long> {
 
+	Iterable<State> findByDescriptionIgnoreCaseLike(String search);
+
+	Iterable<State> findByDescriptionIgnoreCaseContains(String search);
 }
--- src/main/java/org/wecancodeit/restingcitiesstates/StateRestController.java
+++ src/main/java/org/wecancodeit/restingcitiesstates/StateRestController.java
@@ -1,5 +1,33 @@
 package org.wecancodeit.restingcitiesstates;
 
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/states")
 public class StateRestController {
 
+	@Resource
+	private StateRepository stateRepo;
+
+	@RequestMapping("")
+	public Iterable<State> finalAddStates(@RequestParam(defaultValue = "") String search,
+			@RequestParam(defaultValue = "") String advanced) throws InterruptedException {
+		if (search.isEmpty()) {
+			if (advanced.isEmpty()) {
+				return stateRepo.findAll();
+			}
+			return stateRepo.findByDescriptionIgnoreCaseLike(advanced.replace('*', '%'));
+		}
+		return stateRepo.findByDescriptionIgnoreCaseContains(search);
+	}
+
+	@RequestMapping("/{id")
+	public State findOneState(@PathVariable long id) {
+		return stateRepo.findOne(id);
+	}
 }
