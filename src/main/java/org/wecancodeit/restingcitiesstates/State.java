9bfebbfcca3c033a544de18c3b1ac9ae7cd9ef7b
WillieT1986 <WarMad2@Hotmail.com>
2018-03-15 10:40:48 -0400

    Got 1st test to past after some trial and error

--- src/main/java/org/wecancodeit/restingcitiesstates/State.java
+++ src/main/java/org/wecancodeit/restingcitiesstates/State.java
@@ -1,20 +1,31 @@
 package org.wecancodeit.restingcitiesstates;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class State {
 
 	@Id
 	@GeneratedValue
 	private long id;
+	private String stateName;
+
+	public State() {
+
+	}
+
+	public State(String stateName) {
+		this.stateName = stateName;
+	}
 
 	public long getId() {
 		return id;
 	}
 
 	public String getStateName() {
-		return null;
+		return stateName;
 	}
 
 }
--- src/test/java/org/wecancodeit/restingcitiesstates/JpaMappingTest.java
+++ src/test/java/org/wecancodeit/restingcitiesstates/JpaMappingTest.java
@@ -6,8 +6,13 @@ import static org.junit.Assert.assertThat;
 import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
 public class JpaMappingTest {
 
 	@Resource
@@ -18,7 +23,7 @@ public class JpaMappingTest {
 
 	@Test
 	public void shouldSaveAndLoadState() {
-		State state = new State();
+		State state = new State("State Name");
 		state = stateRepo.save(state);
 		long stateId = state.getId();
 
