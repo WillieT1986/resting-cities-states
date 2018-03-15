56205045d8daa9efafb9d2b4a0f78daa4f33a2d7
WillieT1986 <WarMad2@Hotmail.com>
2018-03-15 10:04:43 -0400

    Initial Commit

---
+++ src/main/java/org/wecancodeit/restingcitiesstates/RestingCitiesStatesApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.restingcitiesstates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestingCitiesStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestingCitiesStatesApplication.class, args);
+	}
+}
---
+++ src/test/java/org/wecancodeit/restingcitiesstates/RestingCitiesStatesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.restingcitiesstates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestingCitiesStatesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
